bc672c174d2e5cb9e61c41d5f86b533456cf59ed
Roberto Marcelino Machado <57845086+RobertoMarcelino@users.noreply.github.com>
2021-03-01 18:19:11 -0300

    Update windows_updates.ps1

--- windows_updates.ps1
+++ windows_updates.ps1
@@ -311,10 +311,9 @@ if ($countHidden -gt 0) {
 	& $Sender $Senderarg1 $Senderarg2 $Senderarg3 $Senderarg4 $env:temp$Senderargcountcritical -s "$hostname"
 	& $Sender $Senderarg1 $Senderarg2 $Senderarg3 $Senderarg4 $env:temp$SenderargcountOptional -s "$hostname"
 	& $Sender $Senderarg1 $Senderarg2 $Senderarg3 $Senderarg4 $env:temp$SenderargcountHidden -s "$hostname"
-	& $Sender $Senderarg1 $Senderarg2 $Senderarg3 $Senderarg5 $Senderargupdating $Senderarg6 $Senderarg7 -s "$hostname" 
+	& $Sender $Senderarg1 $Senderarg2 $Senderarg3 $Senderarg5 $Senderargupdating $Senderarg6 $Senderarg7 -s "$hostname"
 	
 	exit $returnStateOK
 }
 
 exit $returnStateUnknown
- 
